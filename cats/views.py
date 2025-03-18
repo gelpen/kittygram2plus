5e3ac6b4ace2434f6ec6790f29dace8adca9958c
Yevgeniy_T <59078119+gelpen@users.noreply.github.com>
2025-03-18 23:19:08 +0500

    ok

--- cats/views.py
+++ cats/views.py
@@ -11,6 +11,8 @@ from .throttling import WorkingHoursRateThrottle
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.pagination import LimitOffsetPagination
 from .pagination import CatsPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 
 class CatViewSet(viewsets.ModelViewSet):
@@ -25,16 +27,32 @@ class CatViewSet(viewsets.ModelViewSet):
     # Если он вернёт False - все запросы будут отклонены
     throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
     # А далее применится лимит low_request
-
+###########################################################################
     # Для любых пользователей установим кастомный лимит 1 запрос в минуту
     # throttle_scope = 'low_request'
-
+#######################################################################
     # pagination_class = PageNumberPagination
     # Даже если на уровне проекта установлен PageNumberPagination
     # Для котиков будет работать LimitOffsetPagination
     # pagination_class = LimitOffsetPagination
     # Вот он наш собственный класс пагинации с page_size=20
-    pagination_class = CatsPagination
+    # pagination_class = CatsPagination
+######################################################################
+    # Указываем фильтрующий бэкенд DjangoFilterBackend
+    # Из библиотеки django-filter
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter) # Добавим в кортеж ещё один бэкенд
+    # Временно отключим пагинацию на уровне вьюсета, 
+    # так будет удобнее настраивать фильтрацию
+    pagination_class = None
+    # Фильтровать будем по полям color и birth_year модели Cat
+    filterset_fields = ('color', 'birth_year')
+    #####################################################################
+    search_fields = ('name',)
+    #####################################################################
+    ordering_fields = ('name', 'birth_year')
+    ###################################################################
+    ordering = ('birth_year',)
 
     def get_permissions(self):
         # Если в GET-запросе требуется получить информацию об объекте
